9f9056330ae1ae5bf987c90a4eb0cce689dfdb8b
SekharMuni <gunapatimunisekhar@gmail.com>
2016-09-18 23:40:07 +0530

    a new line added in main.java in eclipseIDE

--- Main.java
+++ Main.java
@@ -7,5 +7,6 @@ class Main
 		System.out.println("Copying changes froim remote repository to local Repository");
 		System.out.println("creating a local branch");
 		System.out.println("pulling a remote branch to the Remote Repository");
+		System.out.println("eclipse ide created at git")
 	}
 }
