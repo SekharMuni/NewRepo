efa4ca545b7c0b7525ba5db97a78215f542d632b
SekharMuni <gunapatimunisekhar@gmail.com>
2016-09-18 22:55:01 +0530

    a new line added main.java in branch

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ class Main
 		System.out.println("Hello World!");
 		System.out.println("Local to remote repository");
 		System.out.println("Copying changes froim remote repository to local Repository");
+		System.out.println("creating a local branch");
 	}
 }
