7819c437e9b6735d016489ac66987d7cf6060200
SekharMuni <gunapatimunisekhar@gmail.com>
2016-09-18 23:14:18 +0530

    new line is added to main.java in Remote Repository

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ class Main
 		System.out.println("Local to remote repository");
 		System.out.println("Copying changes froim remote repository to local Repository");
 		System.out.println("creating a local branch");
+		System.out.println("pulling a remote branch to the Remote Repository");
 	}
 }
